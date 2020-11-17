542ddd5ddb25a720e95c3779f1af1699136c37ba
mikolajgrzegorzek <mikgrzeg@icloud.com>
2020-11-17 01:37:08 +0100

    Change of location.

--- backend/django_app/scheduler/tests/test_models.py
+++ backend/django_app/scheduler/tests/test_models.py
@@ -1,19 +1,20 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from scheduler.models import User, Instructor, Client, Appointment
+from ..models import User, Instructor, Client, Appointment
+
 
 class UserModelTest(TestCase):
-    @classmethod 
+    @classmethod
     def setUp(cls):
         cls.user1 = User.objects.create_user(email='test@gmail.com',
-                                            first_name='tester',
+                                             first_name='tester',
                                              password='password123',
                                              is_instructor=True)
 
         cls.user2 = User.objects.create_user(email='test2@gmail.com',
-                                                    first_name='tester',
-                                                    password='password123',
-                                                    is_instructor=False)                                             
+                                             first_name='tester',
+                                             password='password123',
+                                             is_instructor=False)
 
     def test_user_creation(self):
         email = 'test1@gmail.com'
@@ -37,9 +38,9 @@ class UserModelTest(TestCase):
         first_name = 'TestName'
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None,
-                                                first_name=first_name,
-                                                password=password,
-                                                is_instructor=True)
+                                                 first_name=first_name,
+                                                 password=password,
+                                                 is_instructor=True)
 
     def test_creating_instructor_when_is_instructor_is_true(self):
         instructor = User.objects.get(email=self.user1.email).profile
@@ -48,5 +49,5 @@ class UserModelTest(TestCase):
 
     def test_creating_instructor_when_is_instructor_is_false(self):
         user = User.objects.get(email=self.user2.email)
-        with self.assertRaises(Instructor.DoesNotExist):   
+        with self.assertRaises(Instructor.DoesNotExist):
             Instructor.objects.get(user=user)
